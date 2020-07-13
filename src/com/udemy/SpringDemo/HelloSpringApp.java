8a2309398cbc217dd9e56fedd6e3bf0066ee2742
harika penukonda <penukondaharika@gmail.com>
2020-07-13 16:15:17 -0500

    Create HelloSpringApp.java

---
+++ src/com/udemy/SpringDemo/HelloSpringApp.java
@@ -0,0 +1,20 @@
+package com.udemy.SpringDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//retrieve bean from the spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		//close the context
+		context.close();
+	}
+
+}
