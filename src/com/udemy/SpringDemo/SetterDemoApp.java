debc00460a18763ac709ac06d1f9782912b8d6de
harika penukonda <penukondaharika@gmail.com>
2020-07-14 11:49:23 -0500

    Update SetterDemoApp.java

--- src/com/udemy/SpringDemo/SetterDemoApp.java
+++ src/com/udemy/SpringDemo/SetterDemoApp.java
@@ -9,10 +9,15 @@ public class SetterDemoApp {
 	// load the spring configuration file
 	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 	// retrieve bean from spring container
-	Coach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+	/* The CricketCoach class has four methods: getDailyWorkout, getDailyFortune, getTeam and setTeam 
+	 * The Coach interface has two methods: getDailyWorkout and getDailyFortune*/
+	CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 	// call methods on the bean
 	System.out.println(theCoach.getDailyWorkout());	
 	System.out.println(theCoach.getDailyFortune());
+	// call the new methods to get the literal values
+	System.out.println(theCoach.getEmailAddress());
+	System.out.println(theCoach.getTeam());
 	// close the context
 	context.close();
 	}
