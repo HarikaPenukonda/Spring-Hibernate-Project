29200938a24cf03717463346b6580bb5fe79d4b6
harika penukonda <penukondaharika@gmail.com>
2020-07-14 10:56:23 -0500

    Create SwimmingCoach.java

---
+++ src/com/udemy/SpringDemo/SwimmingCoach.java
@@ -0,0 +1,16 @@
+package com.udemy.SpringDemo;
+
+public class SwimmingCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout () {
+		return "Do 100 laps daily";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
