4affc6a1e27d38e80753a741d39abfba4f00b3a8
harika penukonda <penukondaharika@gmail.com>
2020-07-14 10:56:25 -0500

    Update TrackCoach.java

--- src/com/udemy/SpringDemo/TrackCoach.java
+++ src/com/udemy/SpringDemo/TrackCoach.java
@@ -2,10 +2,29 @@ package com.udemy.SpringDemo;
 
 public class TrackCoach implements Coach {
 
+	
+	private FortuneService fortuneService;
+	
+	public TrackCoach()
+	{
+		
+	}
+	
+	public TrackCoach(FortuneService fortuneService)
+	{
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Run a hard 5K";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return "Just do it! "+fortuneService.getFortune();
 	}
 
 }
