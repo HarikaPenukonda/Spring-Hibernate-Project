235c4c3f690d8aef725455663419dcae9e7522f9
harika penukonda <penukondaharika@gmail.com>
2020-07-14 10:56:03 -0500

    Update BaseballCoach.java

--- src/com/udemy/SpringDemo/BaseballCoach.java
+++ src/com/udemy/SpringDemo/BaseballCoach.java
@@ -2,10 +2,25 @@ package com.udemy.SpringDemo;
 
 public class BaseballCoach implements Coach {
 	
+	// define a private feild for the dependency
+	private FortuneService fortuneService; // Define feild
+	
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) // Define constructor
+	{
+		this.fortuneService = theFortuneService; 
+	}
+	
 	@Override
 	public String getDailyWorkout()
 	{
 		return "Spending 30 minutes in batting cage";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// use FortuneService to get a fortune
+		return fortuneService.getFortune();
 	}
 
 }
